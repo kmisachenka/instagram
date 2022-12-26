bff0d91139cef18a2ae41fd5c33dff44cc8e04bf
frontcursed <ekvilaizer140@gmail.com>
2022-12-26 11:37:31 +0300

    import components Post and use

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Post from './components/Post.jsx';
 
 function App() {
   return (
     <div className="min-h-full">
       <div>Welcome!</div>
+      <Post />
     </div>
   );
 }
