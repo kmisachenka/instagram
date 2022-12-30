2f9fe825ea24bb2d4d25466eeb1349a94e3ae8de
Kiryl Misachenka <kmisachenka@gmail.com>
2022-12-30 13:38:52 +0400

    updated posts seed

--- src/seed.js
+++ src/seed.js
@@ -1,60 +1,37 @@
-export const users = [
-  {
-    userId: '1',
-    username: 'kiryl_misachenka',
-    fullName: 'Kiryl Misachenka',
-    avatar: '/kiryl_misachenka.jpg',
-    emailAddress: 'kiryl@misachenka.dev',
-    following: ['2'],
-    followers: ['1'],
-    dateCreated: Date.now(),
-  },
-  {
-    userId: '2',
-    username: 'vikulia_ryj',
-    fullName: 'Viktoryia Ryzhkova',
-    emailAddress: 'viktoryia@ryzhkova.dev',
-    following: ['1'],
-    followers: ['2'],
-    dateCreated: Date.now(),
-  },
-];
-
-export function getUserById(userId) {
-  return users.find((user) => user.userId === userId);
-}
-
 export const posts = [
   {
     postId: '1',
     userId: '1',
-    imageSrc: '/images/1.jpg',
-    caption: 'Saint George and the Dragon',
-    likes: ['2'],
+    userName: "Kiry; Misachenka",
+    imageSrc: '/1.jpg',
+    likes: 3,
     comments: [
       {
+        userId: '3',
+        userName: "John Doe",
+        comment: 'Cool!',
+      },
+      {
         userId: '2',
+        userName: "Elon Musk",
         comment: 'Would you mind if I used this picture?',
       },
-    ],
-    userLatitude: '40.7128°',
-    userLongitude: '74.0060°',
+    ],    
     dateCreated: Date.now(),
   },
   {
-    postId: '1',
+    postId: '2',
     userId: '1',
-    imageSrc: '/images/1.jpg',
-    caption: 'Saint George and the Dragon',
-    likes: ['2'],
+    userName: "Elon Musk",
+    imageSrc: '/2.jpg',
+    likes: 1,
     comments: [
       {
-        userId: '2',
-        comment: 'Would you mind if I used this picture?',
+        userId: '1',
+        userName: "Kiryl Misachenka",
+        comment: 'Amazing!',
       },
-    ],
-    userLatitude: '40.7128°',
-    userLongitude: '74.0060°',
+    ],    
     dateCreated: Date.now(),
   },
 ];
