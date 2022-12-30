85d7f3e8b8e33d1c28199911a02ff8e61932b28f
Kiryl Misachenka <kmisachenka@gmail.com>
2022-12-30 13:41:17 +0400

    updated posts seed

--- src/seed.js
+++ src/seed.js
@@ -2,8 +2,9 @@ export const posts = [
   {
     postId: '1',
     userId: '1',
-    userName: "Kiry; Misachenka",
-    imageSrc: '/1.jpg',
+    userName: "Kiryl Misachenka",
+    userAvatar: "/avatars/1.jpg",
+    imageSrc: '/posts/1.jpg',
     likes: 3,
     comments: [
       {
@@ -20,10 +21,11 @@ export const posts = [
     dateCreated: Date.now(),
   },
   {
-    postId: '2',
-    userId: '1',
-    userName: "Elon Musk",
-    imageSrc: '/2.jpg',
+    postId: '2',    
+    userId: '2',
+    userName: "Elon Musk",  
+    userAvatar: "/avatars/2.jpg",
+    imageSrc: '/posts/2.jpg',
     likes: 1,
     comments: [
       {
